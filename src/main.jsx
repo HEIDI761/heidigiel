548ac81b216bd2572dc77aa95285fecefe9772fb
astrosuka <yegresvoselok@gmail.com>
2025-05-08 15:26:14 -0300

    Add LanguageContext and useLanguage hook for language management

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { LanguageProvider } from "./context/LanguageContext.jsx";
 import App from "./App.jsx";
 
 const queryClient = new QueryClient();
@@ -10,9 +11,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <LanguageProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </LanguageProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
