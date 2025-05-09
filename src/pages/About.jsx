3b44163dec473dc924137b568fc606ffb4c9f5a2
sofffja <sofi.efron@gmail.com>
2025-05-09 01:45:24 +0000

    Update About component layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,28 +7,30 @@ export default function About() {
   const { language } = useLanguage();
 
   return (
-    <>
+    <div className="flex flex-col gap-6">
       {data?.bio && (
-        <div>
+        <div className="flex max-w-prose flex-col gap-2">
           <PortableText value={data.bio[language] || data.bio.es} />
         </div>
       )}
-      {data?.contact?.email && (
-        <a href={`mailto:${data?.contact.email}`}>{data.contact.email}</a>
-      )}
-      {data?.contact?.phone && <div>{data.contact.phone}</div>}
-      {data?.contact?.instagram && (
-        <a href={data.contact.instagram}>instagram</a>
-      )}
-      {data?.contact?.spotify && <a href={data.contact.spotify}>spotify</a>}
-      {data?.contact?.links &&
-        data.contact.links.map((link) => (
-          <div key={link._key}>
-            <a href={link.url} target="_blank" rel="noopener noreferrer">
-              {link.title[language] || link.title.es}
-            </a>
-          </div>
-        ))}
-    </>
+      <div className="flex flex-col text-center font-serif uppercase">
+        {data?.contact?.email && (
+          <a href={`mailto:${data?.contact.email}`}>{data.contact.email}</a>
+        )}
+        {data?.contact?.phone && <div>{data.contact.phone}</div>}
+        {data?.contact?.instagram && (
+          <a href={data.contact.instagram}>instagram</a>
+        )}
+        {data?.contact?.spotify && <a href={data.contact.spotify}>spotify</a>}
+        {data?.contact?.links &&
+          data.contact.links.map((link) => (
+            <div key={link._key}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                {link.title[language] || link.title.es}
+              </a>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 }
