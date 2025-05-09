38e31e41c5493c26d9b73248f6ffad9dc2eef30d
astrosuka <yegresvoselok@gmail.com>
2025-05-09 00:16:19 -0300

    Implement data fetching and loading state in Music component

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,7 +1,22 @@
+import { useMusicalProjects } from "../sanity/hooks/getData";
+import Loading from "../components/Loading";
+import useLanguage from "../hooks/useLanguage";
+
 export default function Music() {
+  const { data, isLoading, error } = useMusicalProjects();
+  const { language } = useLanguage();
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
       <div>mis proyectos musicales uwu</div>
+      {data.map((project) => (
+        <div key={project._id} className="flex flex-col gap-2">
+          {project.title[language] || project.title.es}
+        </div>
+      ))}
     </div>
   );
 }
