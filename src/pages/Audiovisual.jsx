c7d61dfe88a84498527724535492216e17d02dae
sofffja <sofi.efron@gmail.com>
2025-05-09 01:45:06 +0000

    Update Audiovisual component layout with improved styling for roles and project types

--- src/pages/Audiovisual.jsx
+++ src/pages/Audiovisual.jsx
@@ -7,18 +7,28 @@ export default function Audiovisual() {
   if (isLoading) return <Loading />;
 
   return (
-    <div>
+    <div className="flex flex-col gap-2 lowercase">
       {filters.roles && (
-        <div className="flex gap-4">
+        <div className="flex flex-wrap">
           {filters.roles.map((role) => (
-            <div key={role._id}>{role.role.es}</div>
+            <div
+              className="bg-secondary text-background border-primary rounded-full border px-2"
+              key={role._id}
+            >
+              {role.role.es}
+            </div>
           ))}
         </div>
       )}
       {filters.audiovisualProjectTypes && (
-        <div className="flex gap-4">
+        <div className="flex flex-wrap">
           {filters.audiovisualProjectTypes.map((type) => (
-            <div key={type._id}>{type.type.es}</div>
+            <div
+              className="bg-accent text-background border-primary rounded-full border px-2"
+              key={type._id}
+            >
+              {type.type.es}
+            </div>
           ))}
         </div>
       )}
