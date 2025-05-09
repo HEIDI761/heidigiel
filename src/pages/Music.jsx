c2ee61d83e0dab7deeaf856c7ad82f16c75a19d5
astrosuka <yegresvoselok@gmail.com>
2025-05-09 01:06:05 -0300

    Refactor Music component to display projects conditionally based on SOLISTA_ID

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -2,6 +2,8 @@ import { useMusicalProjects } from "../sanity/hooks/getData";
 import Loading from "../components/Loading";
 import useLanguage from "../hooks/useLanguage";
 
+const SOLISTA_ID = "1f3fb03a-2431-4977-9753-c80314f61e07";
+
 export default function Music() {
   const { data, isLoading, error } = useMusicalProjects();
   const { language } = useLanguage();
@@ -11,12 +13,24 @@ export default function Music() {
 
   return (
     <div>
-      <div>mis proyectos musicales uwu</div>
-      {data.map((project) => (
-        <div key={project._id} className="flex flex-col gap-2">
-          {project.title[language] || project.title.es}
-        </div>
-      ))}
+      {data.map(
+        (project) =>
+          project._id === SOLISTA_ID && (
+            <div key={project._id}>
+              {project.title[language] || project.title.es}
+            </div>
+          ),
+      )}
+      <div>-----</div>
+      <div>otros proyectos:</div>
+      {data.map(
+        (project) =>
+          project._id != SOLISTA_ID && (
+            <div key={project._id} className="flex flex-col gap-2">
+              {project.title[language] || project.title.es}
+            </div>
+          ),
+      )}
     </div>
   );
 }
