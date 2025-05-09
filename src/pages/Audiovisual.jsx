ce2cb4f9759ccb81e0e95ef3d818b8123e76cbe2
astrosuka <yegresvoselok@gmail.com>
2025-05-08 22:27:20 -0300

    Implement loading state and display audiovisual filters in Audiovisual component

--- src/pages/Audiovisual.jsx
+++ src/pages/Audiovisual.jsx
@@ -1,7 +1,27 @@
+import { useAudiovisualFilters } from "../sanity/hooks/getData";
+import Loading from "../components/Loading";
+
 export default function Audiovisual() {
+  const { data: filters, isLoading } = useAudiovisualFilters();
+
+  if (isLoading) return <Loading />;
+
   return (
     <div>
-      <div>mis proyectos audiovisuales uwu</div>
+      {filters.roles && (
+        <div className="flex gap-4">
+          {filters.roles.map((role) => (
+            <div key={role._id}>{role.role.es}</div>
+          ))}
+        </div>
+      )}
+      {filters.audiovisualProjectTypes && (
+        <div className="flex gap-4">
+          {filters.audiovisualProjectTypes.map((type) => (
+            <div key={type._id}>{type.type.es}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
