635cb16c58cf7a9ef2e50cb7bd8c717f5f717276
astrosuka <yegresvoselok@gmail.com>
2025-05-14 20:36:22 -0300

    Add HighlightedMusicalItem route and handleHighlightClick function for navigation based on _type of highlighted item

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useNavigate } from "react-router";
 import { useAbout } from "./sanity/hooks/getData";
 import useLanguage from "./hooks/useLanguage";
 import About from "./pages/About";
@@ -9,10 +9,23 @@ import MusicalProject from "./pages/MusicalProject";
 import Loading from "./components/Loading";
 import CursorDecoration from "./components/CursorDecoration";
 import Menu from "./components/Menu";
+import HighlightedMusicalItem from "./pages/HighlightedMusicalItem";
 
 function App() {
   const { data, isLoading, error } = useAbout();
   const { language } = useLanguage();
+  const navigate = useNavigate();
+
+  const handleHighlightClick = (highlight) => {
+    console.log(highlight);
+    if (highlight._type === "audiovisualProject") {
+      navigate(`/audiovisual/${highlight.slug}`);
+    } else if (highlight._type === "musicalProject") {
+      navigate(`/musica/${highlight.slug}`);
+    } else if (highlight._type === "musicalItem") {
+      navigate(`/${highlight.slug}`);
+    }
+  };
 
   if (isLoading) return <Loading />;
   if (error) return <div>Error: {error.message}</div>;
@@ -24,9 +37,12 @@ function App() {
       <Menu />
 
       {data?.highlight && (
-        <div className="bg-primary text-background hover:bg-accent fixed right-0 bottom-0 z-50 m-8 flex w-min -rotate-8 cursor-pointer justify-end rounded-full p-8 text-center font-serif text-xl italic transition-all duration-300 hover:scale-105 hover:rotate-3">
+        <button
+          onClick={() => handleHighlightClick(data.highlight)}
+          className="bg-primary text-background hover:bg-accent fixed right-0 bottom-0 z-50 m-8 flex w-min -rotate-8 cursor-pointer justify-end rounded-full p-8 text-center font-serif text-xl italic transition-all duration-300 hover:scale-105 hover:rotate-3"
+        >
           {data.highlight.title[language] || data.highlight.title.es}
-        </div>
+        </button>
       )}
 
       <div className="px-16 pt-26 pb-36">
@@ -37,6 +53,7 @@ function App() {
           <Route path="/audiovisual/:slug" element={<AudiovisualProject />} />
           <Route path="/musica" element={<Music />} />
           <Route path="/musica/:slug" element={<MusicalProject />} />
+          <Route path="/:slug" element={<HighlightedMusicalItem />} />
         </Routes>
       </div>
     </div>
