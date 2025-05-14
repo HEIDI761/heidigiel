d1401848d87d998bb75a9f17c440b47fc9c6e8b7
astrosuka <yegresvoselok@gmail.com>
2025-05-14 18:13:31 -0300

    Add Lightbox functionality to MusicalItem for image display

--- src/components/MusicalItem.jsx
+++ src/components/MusicalItem.jsx
@@ -3,87 +3,116 @@ import { PortableText } from "@portabletext/react";
 import useLanguage from "../hooks/useLanguage";
 import VimeoPlayer from "./VimeoPlayer";
 import Tag from "./Tag";
+import Lightbox from "../components/Lightbox";
+import { AnimatePresence } from "motion/react";
+import useIsMobile from "../hooks/useIsMobile";
 
 export default function MusicalItem({ item }) {
   const { language } = useLanguage();
   const [isDescriptionOpen, setDescriptionIsOpen] = useState(false);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
+  const isMobile = useIsMobile();
 
   return (
-    <div key={item._id} className="flex flex-col gap-2 pb-4">
-      <div className="flex flex-wrap items-center gap-2 text-xs">
-        <Tag tag={item.type.type} key={item.type._id} clickabe={false} />
-        <p className="italic">{item.date.slice(0, 4)}</p>
-      </div>
-      <h2 className="font-serif text-2xl">
-        {item.title[language] || item.title.es}
-      </h2>
-      <img
-        className="rounded-sm"
-        src={item.coverImage.url + "?fm=webp&h=400"}
-        alt={item.title}
-      />
-      {item.images && (
-        <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {item.images.map((image) => (
-            <img
-              key={image._key}
-              src={image.url + "?fm=webp&h=400"}
-              alt={item.title.es}
-              className="rounded-sm"
-            />
-          ))}
+    <>
+      <AnimatePresence>
+        {isLightboxOpen && (
+          <Lightbox
+            key="lightbox"
+            currentImage={currentImage}
+            setIsLightboxOpen={setIsLightboxOpen}
+          />
+        )}
+      </AnimatePresence>
+      <div key={item._id} className="flex flex-col gap-2 pb-4">
+        <div className="flex flex-wrap items-center gap-2 text-xs">
+          <Tag tag={item.type.type} key={item.type._id} clickabe={false} />
+          <p className="italic">{item.date.slice(0, 4)}</p>
         </div>
-      )}
-      {item.vimeoVideos && (
-        <div className="flex flex-col gap-2">
-          {item.vimeoVideos.map((video, index) => (
-            <div key={index}>
-              <VimeoPlayer url={video} />
-            </div>
-          ))}
-        </div>
-      )}
-      {item.customFields && (
-        <div className="text-muted-text flex flex-col">
-          {item.customFields.map((field) => (
-            <div key={field._key} className="flex items-center gap-2">
-              <span className="text-xs uppercase">
-                {field.name[language] || field.name.es}:{" "}
-              </span>
-              <span className="italic">
-                {field.value[language] || field.value.es}
-              </span>
-            </div>
-          ))}
-        </div>
-      )}
-      {item.links && (
-        <div className="flex flex-col gap-2">
-          {item.links.map((link) => (
-            <a
-              key={link._key}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {link.title[language] || link.title.es}
-            </a>
-          ))}
-        </div>
-      )}
-      {item.description && (
-        <button
-          className="bg-tertiary hover:bg-accent text-background size-10 cursor-pointer rounded-full text-2xl"
-          onClick={() => setDescriptionIsOpen(!isDescriptionOpen)}
-        >
-          {isDescriptionOpen ? "-" : "+"}
-        </button>
-      )}{" "}
-      {item.description && isDescriptionOpen && (
-        <PortableText
-          value={item.description[language] || item.description.es}
+        <h2 className="font-serif text-2xl">
+          {item.title[language] || item.title.es}
+        </h2>
+        <img
+          onClick={() => {
+            if (!isMobile) {
+              setCurrentImage(item.coverImage.url + "?fm=webp");
+              setIsLightboxOpen(true);
+            }
+          }}
+          className="cursor-zoom-in rounded-sm"
+          src={item.coverImage.url + "?fm=webp&h=400"}
+          alt={item.title}
         />
-      )}
-    </div>
+        {item.images && (
+          <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 lg:grid-cols-4">
+            {item.images.map((image) => (
+              <img
+                onClick={() => {
+                  if (!isMobile) {
+                    setCurrentImage(image.url + "?fm=webp");
+                    setIsLightboxOpen(true);
+                  }
+                }}
+                key={image._key}
+                src={image.url + "?fm=webp&h=400"}
+                alt={item.title.es}
+                className="cursor-zoom-in rounded-sm"
+              />
+            ))}
+          </div>
+        )}
+        {item.vimeoVideos && (
+          <div className="flex flex-col gap-2">
+            {item.vimeoVideos.map((video, index) => (
+              <div key={index}>
+                <VimeoPlayer url={video} />
+              </div>
+            ))}
+          </div>
+        )}
+        {item.customFields && (
+          <div className="text-muted-text flex flex-col">
+            {item.customFields.map((field) => (
+              <div key={field._key} className="flex items-center gap-2">
+                <span className="text-xs uppercase">
+                  {field.name[language] || field.name.es}:{" "}
+                </span>
+                <span className="italic">
+                  {field.value[language] || field.value.es}
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
+        {item.links && (
+          <div className="flex flex-col gap-2">
+            {item.links.map((link) => (
+              <a
+                key={link._key}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link.title[language] || link.title.es}
+              </a>
+            ))}
+          </div>
+        )}
+        {item.description && (
+          <button
+            className="bg-tertiary hover:bg-accent text-background size-10 cursor-pointer rounded-full text-2xl"
+            onClick={() => setDescriptionIsOpen(!isDescriptionOpen)}
+          >
+            {isDescriptionOpen ? "-" : "+"}
+          </button>
+        )}{" "}
+        {item.description && isDescriptionOpen && (
+          <PortableText
+            value={item.description[language] || item.description.es}
+          />
+        )}
+      </div>
+    </>
   );
 }
