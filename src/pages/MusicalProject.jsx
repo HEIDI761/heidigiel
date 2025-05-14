4b5313f104fbb71dfe1c18a7e0fca103b5a034c8
astrosuka <yegresvoselok@gmail.com>
2025-05-14 18:45:20 -0300

    Add Lightbox functionality to MusicalProject for image display

--- src/pages/MusicalProject.jsx
+++ src/pages/MusicalProject.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { useMusicalItems, useMusicalProject } from "../sanity/hooks/getData";
 import useLanguage from "../hooks/useLanguage";
 import Loading from "../components/Loading";
 import { useParams } from "react-router";
 import MusicalItem from "../components/MusicalItem";
 import { PortableText } from "@portabletext/react";
+import Lightbox from "../components/Lightbox";
+import { AnimatePresence } from "motion/react";
+import useIsMobile from "../hooks/useIsMobile";
 
 export default function MusicalProject() {
   const { slug } = useParams();
@@ -18,26 +22,60 @@ export default function MusicalProject() {
     error: itemsError,
   } = useMusicalItems(slug);
   const { language } = useLanguage();
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
+  const isMobile = useIsMobile();
+
   if (itemsLoading || projectLoading) return <Loading />;
   if (itemsError || projectError) return <div>Error: {itemsError.message}</div>;
 
   return (
-    <div>
-      <h1 className="pb-4 text-center font-serif text-4xl">
-        {project.title[language] || project.title.es}
-      </h1>
-      {project.description && (
-        <div className="mx-auto max-w-prose">
-          <PortableText
-            value={project.description[language] || project.description.es}
+    <>
+      <AnimatePresence>
+        {isLightboxOpen && (
+          <Lightbox
+            key="lightbox"
+            currentImage={currentImage}
+            setIsLightboxOpen={setIsLightboxOpen}
           />
+        )}
+      </AnimatePresence>
+      <div>
+        <h1 className="pb-4 text-center font-serif text-4xl">
+          {project.title[language] || project.title.es}
+        </h1>
+        {project.description && (
+          <div className="mx-auto max-w-prose">
+            <PortableText
+              value={project.description[language] || project.description.es}
+            />
+          </div>
+        )}
+
+        {project.images && (
+          <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-2 lg:grid-cols-4">
+            {project.images.map((image) => (
+              <img
+                onClick={() => {
+                  if (!isMobile) {
+                    setCurrentImage(image.url + "?fm=webp");
+                    setIsLightboxOpen(true);
+                  }
+                }}
+                key={image._key}
+                src={image.url + "?fm=webp&h=400"}
+                alt={project.title.es}
+                className="cursor-zoom-in rounded-sm"
+              />
+            ))}
+          </div>
+        )}
+        <div className="grid grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-4">
+          {items.map((item) => (
+            <MusicalItem key={item._id} item={item} />
+          ))}
         </div>
-      )}
-      <div className="grid grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-4">
-        {items.map((item) => (
-          <MusicalItem key={item._id} item={item} />
-        ))}
       </div>
-    </div>
+    </>
   );
 }
