db345a75aeeca66fd4dfffb557eaa14f0f78a8dc
sofffja <sofi.efron@gmail.com>
2025-05-15 01:39:12 +0000

    Refactor Menu component for improved layout and navigation handling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,58 +1,58 @@
 import useLanguage from "../hooks/useLanguage";
 import { NavLink } from "react-router";
 import LanguageSwitcher from "./LanguageSwitcher";
+import { useLocation } from "react-router";
 
 export default function Menu() {
   const { language } = useLanguage();
+  const location = useLocation();
+
+  console.log(location);
+
   return (
-    <div className="fixed inset-0 z-50 h-min">
-      <nav className="mt-3 grid w-full grid-cols-[1fr_auto_1fr] px-6 font-serif text-3xl lg:text-6xl">
-        <NavLink
-          to="/audiovisual"
-          className={({ isActive }) =>
-            `hover:text-primary text-start uppercase transition-colors hover:italic ${isActive ? "italic" : ""}`
-          }
-        >
-          {language === "es" ? "Audiovisual" : "Audiovisual"}
-        </NavLink>
-        <div className="flex flex-col items-center justify-center pt-0 lg:pt-2">
+    <>
+      <div className="pointer-events-none fixed inset-0 z-50">
+        <nav className="grid w-full grid-cols-[1fr_auto_1fr] items-start gap-12 pt-2 font-serif">
           <NavLink
-            to="/"
+            to="/musica"
             className={({ isActive }) =>
-              `text-2xl lg:text-4xl ${isActive ? "" : ""}`
+              `pointer-events-auto mt-2 justify-self-end rounded-full px-4 text-2xl uppercase transition-all ${isActive ? "bg-primary text-background -rotate-2 italic" : "bg-secondary text-background hover:bg-secondary/50 hover:scale-105 hover:-rotate-2"}`
             }
           >
-            <h1 className="hover:text-accent font-serif italic transition-colors hover:not-italic">
+            {language === "es" ? "Musica" : "Music"}
+          </NavLink>
+          <NavLink
+            to={location.pathname === "/about" ? "/" : "/about"}
+            className="group hover:text-accent pointer-events-auto flex cursor-help flex-col items-center pt-0 transition-colors lg:pt-2"
+          >
+            <h1 className="font-serif text-2xl italic transition-colors lg:text-4xl">
               Heidi Giel
             </h1>
+            <div
+              className={`justify-self-center text-4xl uppercase transition-colors`}
+            >
+              {/* 🪭 */}
+              <img
+                src="https://static.thenounproject.com/png/hand-fan-icon-7565442-512.png"
+                alt=""
+                className="w-10 transition-transform duration-300 group-hover:scale-105 group-hover:rotate-4"
+              />
+            </div>
           </NavLink>
+
           <NavLink
-            to="/about"
+            to="/audiovisual"
             className={({ isActive }) =>
-              `hover:text-primary cursor-help justify-self-center uppercase transition-colors ${isActive ? "underline" : ""}`
+              `pointer-events-auto mt-2 justify-self-start rounded-full px-4 text-2xl uppercase transition-all ${isActive ? "bg-primary text-background rotate-2 italic" : "bg-secondary text-background hover:bg-secondary/50 hover:scale-105 hover:rotate-2"}`
             }
           >
-            {/* 🪭 */}
-            <img
-              src="https://static.thenounproject.com/png/hand-fan-icon-7565442-512.png"
-              alt=""
-              className="w-12 transition-transform duration-300 hover:scale-105 hover:rotate-4"
-            />
+            {language === "es" ? "Audiovisual" : "Audiovisual"}
           </NavLink>
-        </div>
-
-        <NavLink
-          to="/musica"
-          className={({ isActive }) =>
-            `hover:text-primary text-end uppercase transition-colors hover:italic ${isActive ? "italic" : ""}`
-          }
-        >
-          {language === "es" ? "Música" : "Music"}
-        </NavLink>
-      </nav>
-      <div className="bg-tertiary hover:bg-secondary hover:text-background text-muted-text absolute right-0 -mt-6 rounded-l-full pr-2 pl-1.5 text-sm">
+        </nav>
+      </div>
+      <div className="bg-tertiary hover:bg-secondary hover:text-background text-muted-text fixed top-24 right-0 z-50 w-max rounded-l-full pr-2 pl-1.5 text-sm">
         <LanguageSwitcher />
       </div>
-    </div>
+    </>
   );
 }
