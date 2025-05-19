3cb3fe44af354cbba0d29dc88f2c9374d099038d
astrosuka <yegresvoselok@gmail.com>
2025-05-19 20:45:56 -0300

    Update menu icons hover style

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -4,15 +4,13 @@ export default function LanguageSwitcher() {
   const { language, setLanguage } = useLanguage();
 
   return (
-    <div className="">
-      <button
-        className="cursor-pointer uppercase"
-        onClick={() => {
-          setLanguage(language === "es" ? "en" : "es");
-        }}
-      >
-        {language === "es" ? "en" : "es"}
-      </button>
-    </div>
+    <button
+      className="border-background text-background hover:bg-background/80 hover:text-primary flex size-8 cursor-pointer justify-center rounded-[50%] border px-1 pt-2.25 text-center text-xs leading-none uppercase"
+      onClick={() => {
+        setLanguage(language === "es" ? "en" : "es");
+      }}
+    >
+      {language === "es" ? "en" : "es"}
+    </button>
   );
 }
