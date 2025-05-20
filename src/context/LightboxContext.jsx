d90e88056178867bffb3479d14e3ae5a9519b9a4
astrosuka <yegresvoselok@gmail.com>
2025-05-19 21:44:27 -0300

    Set image height to 1500 to improve performance

--- src/context/LightboxContext.jsx
+++ src/context/LightboxContext.jsx
@@ -7,7 +7,7 @@ const LightboxProvider = ({ children }) => {
   const [currentImage, setCurrentImage] = useState(null);
 
   const openLightbox = (image) => {
-    setCurrentImage(image.url + "?fm=webp");
+    setCurrentImage(image.url + "?h=1500&fm=webp");
     setIsLightboxOpen(true);
   };
   return (
