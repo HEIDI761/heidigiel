efff61cfe360b7c725ebd186f2084a746ba7f292
sofffja <sofi.efron@gmail.com>
2025-05-20 00:20:16 +0000

    Wrap MusicalItem component in a styled container for improved layout and presentation

--- src/pages/HighlightedMusicalItem.jsx
+++ src/pages/HighlightedMusicalItem.jsx
@@ -11,8 +11,10 @@ export default function HighlightedMusicalItem() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <>
-      <MusicalItem item={data} />
-    </>
+    <div className="from-background fixed inset-0 flex h-screen w-full items-center justify-center bg-radial/oklab from-0% to-transparent to-70% bg-fixed">
+      <div className="bg-background max-h-3/4 max-w-2xl overflow-y-auto border p-4 pt-6">
+        <MusicalItem item={data} />
+      </div>
+    </div>
   );
 }
