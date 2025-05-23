5619be8064487d452405eeae6c7c3e8b68ef33a8
astrosuka <yegresvoselok@gmail.com>
2025-05-23 14:47:51 -0300

    Comment out components to avoid App breaking while refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import useLanguage from "./hooks/useLanguage";
 import useLightbox from "./hooks/useLightbox";
 import About from "./pages/About";
 import Audiovisual from "./pages/Audiovisual";
-import Music from "./pages/Music";
-import AudiovisualProject from "./pages/AudiovisualProject";
-import MusicalProject from "./pages/MusicalProject";
+// import Music from "./pages/Music";
+// import AudiovisualProject from "./pages/AudiovisualProject";
+// import MusicalProject from "./pages/MusicalProject";
 import Loading from "./components/Loading";
 import CursorDecoration from "./components/CursorDecoration";
 import Menu from "./components/Menu";
-import HighlightedMusicalItem from "./pages/HighlightedMusicalItem";
+// import HighlightedMusicalItem from "./pages/HighlightedMusicalItem";
 import Ornaments from "./components/Ornaments";
 import Lightbox from "./components/Lightbox";
 import { AnimatePresence } from "motion/react";
@@ -52,24 +52,24 @@ function App() {
 
       <Menu />
 
-      {data?.highlight && (
-        <button
-          onClick={() => handleHighlightClick(data.highlight)}
-          className="bg-primary hover:bg-accent font-serif-italic fixed right-0 bottom-2 z-50 m-8 flex max-w-2xs -rotate-8 cursor-pointer justify-end rounded-full p-8 text-center text-xl text-white transition-all duration-300 hover:scale-105 hover:rotate-3"
-        >
-          {data.highlight.title[language] || data.highlight.title.es}
-        </button>
-      )}
+      {/* {data?.highlight && ( */}
+      {/*   <button */}
+      {/*     onClick={() => handleHighlightClick(data.highlight)} */}
+      {/*     className="bg-primary hover:bg-accent font-serif-italic fixed right-0 bottom-2 z-50 m-8 flex max-w-2xs -rotate-8 cursor-pointer justify-end rounded-full p-8 text-center text-xl text-white transition-all duration-300 hover:scale-105 hover:rotate-3" */}
+      {/*   > */}
+      {/*     {data.highlight.title[language] || data.highlight.title.es} */}
+      {/*   </button> */}
+      {/* )} */}
 
       <div className="px-8 pt-32 pb-36">
         <Routes>
           <Route path="/" element={null} />
           <Route path="/about" element={<About />} />
           <Route path="/audiovisual" element={<Audiovisual />} />
-          <Route path="/audiovisual/:slug" element={<AudiovisualProject />} />
-          <Route path="/musica" element={<Music />} />
-          <Route path="/musica/:slug" element={<MusicalProject />} />
-          <Route path="/:slug" element={<HighlightedMusicalItem />} />
+          {/* <Route path="/audiovisual/:slug" element={<AudiovisualProject />} /> */}
+          {/* <Route path="/musica" element={<Music />} /> */}
+          {/* <Route path="/musica/:slug" element={<MusicalProject />} /> */}
+          {/* <Route path="/:slug" element={<HighlightedMusicalItem />} /> */}
         </Routes>
       </div>
     </div>
