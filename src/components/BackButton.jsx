59a6894066af36abe1f667b7c9f56efdd80e1131
sofffja <sofi.efron@gmail.com>
2025-05-25 20:06:24 +0000

    Update BackButton component styling

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -8,7 +8,7 @@ export default function BackButton() {
       onClick={() => {
         window.history.back();
       }}
-      className="font-serif-italic group bg-background border-background-dim hover:bg-text hover:text-background flex items-center border border-l-0 py-2 pr-4 pl-3 text-sm uppercase transition-colors duration-500"
+      className="shadow-background-dim group bg-background border-background-dim hover:bg-text hover:text-background flex cursor-pointer items-center border border-l-0 py-2 pr-4 pl-3 text-sm uppercase shadow-md transition-colors duration-500"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
