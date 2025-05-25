2006951dbc38061194ba574d40253a108eda3eb9
sofffja <sofi.efron@gmail.com>
2025-05-25 08:02:17 +0000

    Update BackButton styling with hover effects and transitions

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -8,7 +8,7 @@ export default function BackButton() {
       onClick={() => {
         window.history.back();
       }}
-      className="font-serif-italic bg-background hover:bg-tertiary fixed top-24 left-0 flex items-center border border-l-0 px-2 text-sm uppercase transition-all hover:scale-105"
+      className="font-serif-italic group bg-background border-background-dim hover:bg-text hover:text-background flex items-center border border-l-0 py-2 pr-4 pl-3 text-sm uppercase transition-colors duration-500"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -16,6 +16,7 @@ export default function BackButton() {
         viewBox="0 -960 960 960"
         width="16px"
         fill="var(--color-text)"
+        className="group-hover:fill-background mr-2 h-4 w-4 transition-colors duration-500"
       >
         <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
       </svg>
