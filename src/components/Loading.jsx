6ba920c2b744c64a4976930a8de01f225b8b5113
astrosuka <yegresvoselok@gmail.com>
2025-05-25 23:06:03 -0300

    Improve loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <div>...</div>;
+  return (
+    <div className="bg-background/30 fixed inset-0 z-100 flex h-screen w-screen items-center justify-center">
+      <div className="border-text h-12 w-12 animate-spin rounded-full border-2 border-solid border-t-transparent"></div>
+    </div>
+  );
 }
