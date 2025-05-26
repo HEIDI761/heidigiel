6796ebe409fde3f0f03140be582e387ca0b56b00
sofffja <sofi.efron@gmail.com>
2025-05-26 05:52:51 +0000

    Animate About section text opacity

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import { useAbout } from "../sanity/hooks/getData";
 import useLanguage from "../hooks/useLanguage";
 import { PortableText } from "@portabletext/react";
-import TextContainer from "../components/TextContainer";
+import { motion } from "motion/react";
 
 export default function About() {
   const { data } = useAbout();
@@ -12,9 +12,14 @@ export default function About() {
       <div className="from-background via-secondary/70 fixed inset-0 -z-10 h-screen w-full bg-radial to-transparent bg-fixed mix-blend-overlay" />
 
       {data?.bio && (
-        <div className="mx-auto max-w-prose">
+        <motion.div
+          className="mx-auto max-w-prose"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
           <PortableText value={data.bio[language] || data.bio.es} />
-        </div>
+        </motion.div>
       )}
     </div>
   );
