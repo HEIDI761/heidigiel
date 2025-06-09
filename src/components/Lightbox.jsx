1b4366edd576322ba7a9c997fb80e19c77a5d259
astrosuka <yegresvoselok@gmail.com>
2025-06-09 15:10:06 -0300

    Add useIsMobile hook to Lightbox for responsive positioning of projecgt link button and add padding

--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -5,12 +5,14 @@ import useLightbox from "../hooks/useLightbox";
 import { NavLink } from "react-router";
 import useLanguage from "../hooks/useLanguage";
 import { useEffect } from "react";
+import useIsMobile from "../hooks/useIsMobile.jsx";
 
 export default function Lightbox() {
   const [imageLoaded, setImageLoaded] = useState(false);
   const { currentImage, setIsLightboxOpen, relatedProject, setRelatedProject } =
     useLightbox();
   const { language } = useLanguage();
+  const isMobile = useIsMobile();
 
   const handleClick = useCallback(() => {
     setIsLightboxOpen(false);
@@ -35,7 +37,7 @@ export default function Lightbox() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="no-doc-scroll fixed inset-0 z-100 flex h-screen w-screen cursor-zoom-out items-center justify-center backdrop-brightness-20 backdrop-grayscale-100"
+      className="no-doc-scroll fixed inset-0 z-100 flex h-screen w-screen cursor-zoom-out items-center justify-center p-4 backdrop-brightness-20 backdrop-grayscale-100"
       onClick={() => handleClick()}
     >
       {!imageLoaded && (
@@ -52,7 +54,7 @@ export default function Lightbox() {
       {relatedProject?.slug && (
         <NavLink
           to={`/${relatedProject.audiovisualProjectType ? "audiovisual" : "musica"}/${relatedProject.slug.current}`}
-          className="shadow-background-dim group bg-background border-background-dim hover:bg-text hover:text-background fixed top-0 flex cursor-pointer items-center border border-l-0 py-2 pr-4 pl-3 text-sm uppercase shadow-md transition-colors duration-500"
+          className={`shadow-background-dim group bg-background border-background-dim hover:bg-text hover:text-background fixed flex cursor-pointer items-center border border-l-0 py-2 pr-4 pl-3 text-sm uppercase shadow-md transition-colors duration-500 ${isMobile ? "bottom-4" : "top-4"}`}
         >
           {language === "es" ? "ver mas: " : "see more: "}
           {relatedProject.title[language] || relatedProject.title.es}
