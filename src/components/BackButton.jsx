efbf5ebbe978c1a2d2a57772c2ecd3fbefd74c51
sofffja <sofi.efron@gmail.com>
2025-07-10 19:24:08 +0000

    Fix back button border

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -8,7 +8,7 @@ export default function BackButton({ onClick }) {
       onClick={() => {
         onClick();
       }}
-      className="shadow-background-dim group bg-background border-background-dim hover:bg-text hover:text-background flex cursor-pointer items-center border border-l-0 py-2 pr-4 pl-3 text-sm uppercase shadow-md transition-colors duration-500"
+      className="shadow-background-dim group bg-background border-background-dim hover:bg-text hover:text-background flex cursor-pointer items-center border py-2 pr-4 pl-3 text-sm uppercase shadow-md transition-colors duration-500"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
